a1cddd09c5a743daaea84144b4f934cc036a4de5
jix-coder <rm2340383@gmail.com>
2026-08-31 16:56:41 +0530

    Time: 1 ms (77.68%), Space: 46.6 MB (63.37%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+       int slow =0;
+       
+
+       for(int i=1; i<nums.length; i++){
+        if(nums[slow] != nums[i])
+            slow++;
+            nums[slow] = nums[i];
+            
+        }
+       return slow+1;
+       }
+    
+}
